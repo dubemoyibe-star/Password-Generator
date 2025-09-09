3c7f5f3cd6aa74353b9201b940fb2f98672c90ae
dubemoyibe-star <dubemoyibe@gmail.com>
2025-09-09 10:45:23 +0100

    a password generator
    generates a secure password

---
+++ index.js
@@ -0,0 +1,27 @@
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+let input1El = document.getElementById("input1")
+let input2El = document.getElementById("input2")
+isclicked = false
+
+function generate(){
+    input1El.value = ""
+    input2El.value = ""
+    
+for (let i = 0; i < 12; i++){
+    let randomNumber = Math.floor(Math.random()*characters.length);
+    let randomNumber2 = Math.floor(Math.random()*characters.length);
+    let password1 =  characters[randomNumber];
+    let password2 =  characters[randomNumber2];
+    input1El.value += password1;
+    input2El.value += password2 ;
+    isclicked = true
+}
+}
+
+
+
+
+
+
+
